27a472ad4a0a5ccd2bfad4a84ffc0c2787dd9c1a
Beny <b.ntambwe97@gmail.com>
2023-01-03 18:08:30 +0100

    Google places autocomplete

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,73 @@
-import { View, Text } from 'react-native'
+import {View, Text} from 'react-native'
 import React from 'react'
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
-export default function SearchBar() {
-  return (
-    <View>
-        
-    </View>
-  )
+export default function SearchBar({cityHandler} : {
+    cityHandler: any
+}) {
+    return (
+        <View
+            style={{
+            marginTop: 15,
+            flexDirection: 'row'
+        }}>
+            <GooglePlacesAutocomplete
+                placeholder='Search'
+                query={{
+                key: "AIzaSyBkfSuG-jyDRWnfC6FJqshnki840_vXdnM"
+            }}
+            currentLocation={true}
+            currentLocationLabel='Current location'
+                onPress={(data, details = null) => {
+                console.log(data.description);
+                const city = data
+                    .description
+                    .split(",")[0];
+                cityHandler(city);
+            }}
+                styles={{
+                textInput: {
+                    backgroundColor: '#eee',
+                    borderRadius: 20,
+                    fontWeight: '700',
+                    marginTop: 7
+                },
+                textInputContainer: {
+                    backgroundColor: '#eee',
+                    borderRadius: 50,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginRight: 10
+                }
+            }}
+                renderLeftButton={() => (
+                <View style={{
+                    marginLeft: 10
+                }}>
+                    <Ionicons name="location-sharp" size={24}/>
+                </View>
+            )}
+                renderRightButton={() => (
+                <View
+                    style={{
+                    flexDirection: 'row',
+                    marginRight: 8,
+                    backgroundColor: 'white',
+                    padding: 9,
+                    borderRadius: 30,
+                    alignItems: 'center'
+                }}>
+                    <AntDesign
+                        name="clockcircle"
+                        size={11}
+                        style={{
+                        marginRight: 6
+                    }}/>
+                    <Text>Search</Text>
+                </View>
+            )}/>
+        </View>
+    )
 }
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -2,14 +2,16 @@ import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
 
 export default function Home() {
   return (
     <SafeAreaView style={{backgroundColor:'#eee', flex: 1}}>
       <View style={{backgroundColor: 'white', padding: 15}}>
         <HeaderTabs activeTab={undefined} setActiveTab={undefined}/>
-        <SearchBar />
+        <SearchBar cityHandler={undefined} />
       </View>
+      <Categories />
     </SafeAreaView>
   )
 }
